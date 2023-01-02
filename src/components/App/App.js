b3401275c63302a3a58d9d08a1f40c4d48eb1b03
James Wasmer <jameswasmer@gmail.com>
2023-01-02 16:32:07 -0500

    render fetched tricks on main page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,33 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      tricks: []
+    }
+  }
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/tricks')
+        .then(response => response.json())
+        .then(data => {
+          this.setState({
+            tricks: data
+          })
+        })
+    }
+
   render() {
     return (
       <div className="App">
         <h1>Sick Trick Wish List</h1>
+        <ul>
+          {this.state.tricks.map(trick => {
+            return <li>stance: {trick.stance}, name: {trick.name}, obstacle: {trick.obstacle}, tutorial: {trick.tutorial}, id: {trick.id}</li>
+          })}
+        </ul>
       </div>
     );
   }
 }
-
 export default App;
